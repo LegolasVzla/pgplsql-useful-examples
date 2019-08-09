a6bfcb957463cb168ff2d6724b7dacca2ba8e7e2
Manuel Carrero <mcomercio.web@gmail.com>
2019-08-08 21:47:05 -0400

    Improvement in reverse_geocoding function related with not found places

--- plpython3u-examples/temporal_schema/functions/udf_reverse_geocoding_geopy.sql
+++ plpython3u-examples/temporal_schema/functions/udf_reverse_geocoding_geopy.sql
@@ -11,25 +11,39 @@ AS $function$
     import site
     site.addsitedir('/your_virtualenv_path/lib/pythonX.Y/site-packages')
     from geopy.geocoders import Nominatim
+    from geopy.exc import GeocoderTimedOut
     json_resulting = {}
-    geolocator = Nominatim(user_agent="Ibeen")
-    #location = geolocator.reverse("10.4823307, -66.861713")
-    location = geolocator.reverse(param_latitude+", "+param_longitude)
+    geolocator = Nominatim(user_agent="My_Django_Google_Map_App",timeout=3)
     try:
+        location = geolocator.reverse(param_latitude+", "+param_longitude)
+        #location = geolocator.reverse("10.4823307, -66.861713")
         if(location):
-            json_resulting['country_name']=location.raw['address']['country']
-            json_resulting['state_name']=location.raw['address']['state']
-            json_resulting['city_name']=location.raw['address']['county']
-            json_resulting['postal_code']=location.raw['address']['postcode']
-            json_resulting['road']=location.raw['address']['postcode']
-            json_resulting['full_address']=location.address
-    except Exception as e:    
-        json_resulting['country_name']="undefined"
-        json_resulting['state_name']="undefined"
-        json_resulting['city_name']="undefined"
-        json_resulting['postal_code']="undefined"
-        json_resulting['road']="undefined"
-        json_resulting['full_address']="undefined"
+            try:
+                json_resulting['country_name']=location.raw['address']['country']
+                json_resulting['country_code']=location.raw['address']['country_code'].upper()
+            except Exception as e:
+                json_resulting["country_name"]="undefined"
+                json_resulting["country_code"]="undefined"
+            try:
+                json_resulting['state_name']=location.raw['address']['state']
+            except Exception as e:
+                json_resulting["state_name"]="undefined"
+            try:
+                json_resulting['city_name']=location.raw['address']['city']
+            except Exception as e:
+                json_resulting["city_name"]="undefined"
+            try:
+                json_resulting['postal_code']=location.raw['address']['postcode']
+            except Exception as e:
+                json_resulting["postal_code"]="undefined"
+            try:
+                json_resulting['full_address']=location.raw['display_name']
+            except Exception as e:
+                json_resulting['full_address']="undefined"
+    except (GeocoderTimedOut) as e:    
+        print("Error: geocode failed on input %s with message %s"%(param_latitude," ",param_longitude, e.message))
+        for i,j in json_resulting.items():
+            json_resulting[i] = "undefined"
 
     return json_resulting
 
