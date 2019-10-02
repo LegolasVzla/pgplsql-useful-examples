85119c23d2d615ea61b78eb9c70a0668ec3aa4f9
Manuel Carrero <mcomercio.web@gmail.com>
2019-10-01 20:56:36 -0400

    Adding service to get only your spots

--- tree-postgis-queries/temporal_schema/functions/udf_spot_profile_get.sql
+++ tree-postgis-queries/temporal_schema/functions/udf_spot_profile_get.sql
@@ -1,14 +1,10 @@
--- DROP FUNCTION public.udf_spots_nearby_within_X_kilometers_from_current_user_position(integer, double precision, double precision);
-CREATE OR REPLACE FUNCTION public.udf_spots_nearby_within_X_kilometers_from_current_user_position(
-    param_user_id integer,
-    param_lat double precision,
-    param_long double precision)
+-- DROP FUNCTION public.udf_spots_profile_get(integer);
+CREATE OR REPLACE FUNCTION public.udf_spots_profile_get(param_user_id integer)
   RETURNS json AS
 $BODY$
 
 DECLARE
 
-  local_max_distance decimal = 2000.0;     -- Default max distance: 2 km or you could receive it as a parameter
   aux_tree_returning varchar = '';
   status_value varchar = 'fail';
   data_value json = '{ "User_id": "' || param_user_id || ' not found" }';
@@ -19,7 +15,7 @@ DECLARE
 
   /*
   -- To Test:
-    SELECT udf_spots_nearby_within_X_kilometers_from_current_user_position(6,10.4823307,-66.861713);
+    select public.udf_spots_profile_get(260);
   */
 
   -- Prevention SQL injection
@@ -40,14 +36,15 @@ DECLARE
   status_value = 'success';
 
   DROP TABLE IF EXISTS temporal_spots_table;
+
   -- Create a temporary table to store nearby places
   CREATE TEMPORARY TABLE IF NOT EXISTS temporal_spots_table (
         id integer,
         users_id integer,
         name character varying,
         remarks character varying,
-        reference_point character varying,        
         review character varying,
+        reference_point character varying,
         lat double precision,
         long double precision,
         country_name character varying,
@@ -57,16 +54,16 @@ DECLARE
         full_address character varying,
         is_privated boolean DEFAULT false,
         is_active boolean DEFAULT true,
-        created_at timestamp without time zone        
+        created_at timestamp without time zone
   );
 
   INSERT INTO temporal_spots_table(
     id,
     users_id,
-    name,
+	name,
     remarks,
-    reference_point,
     review,
+    reference_point,
     lat,
     long,
     country_name,
@@ -76,16 +73,16 @@ DECLARE
     full_address,
     is_privated,
     is_active,
-    created_at    
+    created_at
   )
-  -- Get the places within 5 km's from the current position where you are, using PostGIS
+  -- Get all the spots from the current user
   SELECT
     s.id,
     s.users_id,
     s.name,
     s.remarks,
-    s.reference_point,
     s.review,
+    s.reference_point,
     s.lat,
     s.long,
     s.country_name,
@@ -103,21 +100,7 @@ DECLARE
     INNER JOIN entity_statuses es
       ON es.id = ss.entity_status_id
   WHERE
-    s.users_id IN (
-      -- Get spot of your friends
-    SELECT DISTINCT
-      friendable_id
-    FROM
-      friendships
-    WHERE 
-      (friendable_id = param_user_id OR
-      friend_id = param_user_id) AND
-      status = 2 -- Are friends
-    ) OR s.users_id = param_user_id
-    AND 
-    s.lat != param_lat
-    AND
-    s.long != param_long
+    s.users_id = param_user_id
     AND
     ss.id = 5 -- Activo
     AND
@@ -134,8 +117,24 @@ DECLARE
     NOT ss.is_deleted
     AND
     NOT es.is_deleted
-    AND
-    ST_DistanceSphere("s"."position", ST_GeomFromEWKB(ST_MakePoint(param_long,param_lat)::bytea)) <= (local_max_distance::float);
+   GROUP BY
+    s.id,
+    s.users_id,
+    s.name,
+    s.remarks,
+    s.review,
+    s.reference_point,
+    s.lat,
+    s.long,
+    s.country_name,
+    s.state_name,
+    s.city_name,
+    ss.description,
+    s.full_address,
+    s.is_privated,
+    s.is_active,
+    s.created_at   
+    ;
 
     -- Only for temporal_spots_table test purpose 
     /*
@@ -157,7 +156,7 @@ DECLARE
       temporal_spots_table
     ) THEN
 
-      --RAISE NOTICE 'Were found places near where you are';
+      RAISE NOTICE 'Were found places';
 
       SELECT JSON_AGG(a.*) INTO STRICT aux_tree_returning
       FROM (
@@ -222,7 +221,6 @@ DECLARE
     "totalSpots": 0,
     "spotsData":
       [{
-          "totalSpots": null,
           "spotId": null,
           "spotName": null,
           "remarks": null,
@@ -245,7 +243,7 @@ DECLARE
         }]
       }]';
 
-    --RAISE NOTICE 'Were not found places near where you are';
+    RAISE NOTICE 'Were not found places';
 
   END IF;
 
@@ -254,12 +252,12 @@ DECLARE
     RETURN param_json_returning;
 
   DROP TABLE IF EXISTS temporal_spots_table;
-  --COMMIT;
+  COMMIT;
 
 END;
 
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION public.udf_spots_nearby_within_X_kilometers_from_current_user_position(integer, double precision, double precision)
+ALTER FUNCTION public.udf_spots_profile_get(integer)
   OWNER TO postgres;
